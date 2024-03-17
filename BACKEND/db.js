c5fe6e4cb917354dbf8e26651e9c045ab8c12be8
Rohit-GHacc <rohitchugh142001@gmail.com>
2024-03-17 19:46:44 +0530

    Setting up express server and connection with mongo using mongoose and node js ignoring node_modules files on .gitignore npm install mongoose, express, nodemon etc.

---
+++ BACKEND/db.js
@@ -0,0 +1,26 @@
+// const mongoose = require('mongoose');
+
+// const mongoURI = 'mongodb://127.0.0.1:27017/?directConnection=true';
+
+// mongoose.connect(mongoURI,{useNewUrlParser: true, useUnifiedTopology: true})
+// const db = mongoose.connection;
+// db.on('error',console.error.bind('error connecting to db'));
+// db.once('open', () => {
+//     console.log('Connected to mongo successfully');
+// });
+// module.exports = db;
+
+const mongoose = require('mongoose');
+
+const mongoURI = 'mongodb://127.0.0.1:27017/?directConnection=true';
+
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log('Connected to MongoDB successfully');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+};
+
+module.exports = connectToMongo;
---
+++ BACKEND/index.js
@@ -0,0 +1,11 @@
+const connectToMongo = require('./db');
+const express = require('express')
+connectToMongo();
+const app = express();
+const port = 3001;
+app.get('/', (req,res)=>{
+     res.send('Hello world');
+})
+app.listen(port,()=>{
+    console.log(`Server started at port ${port}`)
+})
