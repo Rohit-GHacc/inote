cd8f79b63078d2704ee384283c02bb3a4005f1ba
Rohit-GHacc <rohitchugh142001@gmail.com>
2024-03-18 16:50:08 +0530

    $ changed post request and creating a user to async await requests

--- BACKEND/routes/auth.js
+++ BACKEND/routes/auth.js
@@ -1,24 +1,39 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/Users')
-const { body, validationResult }= require('express-validator')
-router.post('/',[
-    body('name','Enter a valid name').isLength({min:3}),
-    body('email','Enter a valid email').isEmail(),
-    body('password').isLength({min:5})
-],(req,res)=>{
+const { body, validationResult } = require('express-validator')
+router.post('/createuser', [
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password').isLength({ min: 5 })
+], async (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()});
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
-    const user = User(req.body);
-    user.save()
-    // User.create(req.body)
-    // .then(user=>res.json(user))
-    .catch(err=> {console.log(err)
-    res.json({error: 'Please enter a unique value for email',message: err.message})})
-    console.log(req.body);
-    // res.json(req.body)
+    try {
+        // Check whether use with this email already exists
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "Sorry user with this email already exists." })
+        }
+        else {
+            // const user = User(req.body);
+            // user.save()
+            // User.create(req.body)
+            // .then(user=>res.json(user))
+            // .catch(err=> {console.log(err)
+            // res.json({error: 'Please enter a unique value for email',message: err.message})})
+            // console.log(req.body);
+            // res.json(req.body)
+
+            user = await User.create(req.body);
+            res.json(user)
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error occurred");
+    }
 });
 
 module.exports = router;
