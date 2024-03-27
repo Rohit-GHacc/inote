bc9575152eac83d5723b5f6415e95e35fe68758e
Rohit-GHacc <rohitchugh142001@gmail.com>
2024-03-27 09:32:18 +0530

    Adding fontawesome icons delete and edit

--- src/App.js
+++ src/App.js
@@ -7,22 +7,23 @@ import NavBar from './components/NavBar'
 import About from './components/About'
 import Home from './components/Home'
 import NoteState from "./context/notes/NoteState";
-
+import Alert from "./components/Alert";
 function App() {
   return (
     <>
-    <NoteState>
-    <BrowserRouter>
-    <NavBar/>
-    <div className = 'container'>
-    <Routes>
-      <Route exact path='/' element = {<Home/>}/>
-      <Route exact path='/about' element = {<About/>}/>
+      <NoteState>
+        <BrowserRouter>
+          <NavBar />
+          <Alert />   
+          <div className='container'>
+            <Routes>
+              <Route exact path='/' element={<Home />} />
+              <Route exact path='/about' element={<About />} />
 
-    </Routes>
-    </div>
-    </BrowserRouter>
-    </NoteState>
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </NoteState>
     </>
   );
 }
